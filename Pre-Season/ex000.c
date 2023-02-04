6c85429f95291f9cc78055c32fc7700fa3800642
theEmperorofDaiViet <Khiet.To.Development@gmail.com>
2023-02-04 23:52:44 +0700

    first commit

---
+++ Pre-Season/ex000.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// In ra màn hình dòng chữ: Hello PTIT.
+
+// INPUT
+// Không có dữ liệu vào
+// OUTPUT
+// Hello PTIT.
+
+int main(){
+    printf("Hello PTIT.");
+    return 0;
+}
